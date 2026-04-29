08d8ed06ac36b09e5eac841d78d017233b01ffda
Jose Garcia <garciacolin@hotmail.com>
2026-04-28 19:23:43 -0600

    add User model and relationships with loan

--- loans/app/db/models.py
+++ loans/app/db/models.py
@@ -7,6 +7,19 @@ from sqlalchemy.orm import relationship
 # 📁 Local imports
 from .database import Base
 
+class User(Base):
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True, index=True)
+    email = Column(String, unique=True, index=True, nullable=False)
+    first_name = Column(String, nullable=False)
+    last_name = Column(String, nullable=False)
+    hashed_password = Column(String, nullable=False)
+    is_active = Column(Boolean, default=True)
+    role =Column(String)
+
+    loans = relationship("Loan", back_populates="owner")
+
 class Loan(Base):
     __tablename__ = "loans"
 
@@ -14,8 +27,10 @@ class Loan(Base):
     name = Column(String)
     amount = Column(Integer)
     date = Column(Date)
+    owner_id = Column(Integer, ForeignKey("users.id"))
 
-    payments = relationship("Payment", back_populates="loan", cascade="all, delete")
+    owner = relationship("User", back_populates="loans")
+    payments = relationship("Payment", back_populates="loan", cascade="all, delete-orphan")
 
 class Payment(Base):
     __tablename__ = "payments"
@@ -23,8 +38,7 @@ class Payment(Base):
     payment_id = Column(Integer, primary_key=True, index=True)
     payment_number = Column(Integer)
     payment_date = Column(Date)
-    paid = Column(Boolean)
+    paid = Column(Boolean, default=False)
 
     loan_id = Column(Integer, ForeignKey("loans.id"))
-
     loan = relationship("Loan", back_populates="payments")
