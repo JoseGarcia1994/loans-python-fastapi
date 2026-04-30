c353320cfcecf5855da9c2e3c086afffbe8d6ca4
Jose Garcia <garciacolin@hotmail.com>
2026-04-29 20:10:59 -0600

    feat(auth): implement user authentication logic and connect to token endpoint

--- loans/app/api/routers/auth.py
+++ loans/app/api/routers/auth.py
@@ -1,12 +1,14 @@
 # 📦 Standard library
+from typing import Annotated
 
 # 🌐 Third-party
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, HTTPException
 from fastapi.security import OAuth2PasswordRequestForm
-from typing import Annotated
+from starlette import status
 
 # 📁 Local imports
 from ..deps import db_dependency
+from ...services.auth_service import authenticate_user
 
 router = APIRouter()
 
@@ -15,4 +17,10 @@ async def login_for_access_token(
         form_data: Annotated[OAuth2PasswordRequestForm, Depends()],
         db: db_dependency
 ):
-    return form_data.username
+    user = authenticate_user(form_data.username, form_data.password, db)
+    if not user:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Invalid credentials"
+        )
+    return user
